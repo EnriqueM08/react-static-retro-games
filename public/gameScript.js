bd4a641b4cc02d527c9bf91b983c9562d8f9e592
Enrique Mata <eric.mataiii@gmail.com>
2023-11-07 20:19:58 -0600

    Fix to work with react

---
+++ public/gameScript.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    EJS_player = '#game';
+    EJS_core = 'gba';  
+    EJS_gameUrl = './pkmRed.gba';
+    EJS_pathtodata = 'https://cdn.jsdelivr.net/gh/ethanaobrien/emulatorjs@main/data/';
+});
--- src/App.js
+++ src/App.js
@@ -1,1 +1,31 @@
+import song from './song1.mp3';
+import './App.css';
+// import page from './iframe_window.html';
 
+function App() {
+  return (
+    <div className="App">
+        <div className="music-player">
+            <audio id="audio-player" controls>
+                <source src={song} type="audio/mpeg"/>
+                Your browser does not support the audio element.
+            </audio>
+            <div class="song-info">
+                <h2>Song Title</h2>
+                <p>Artist Name</p>
+            </div>
+        </div>
+        <script src="script.js"></script>
+        <iframe id="frame" 
+            src={process.env.PUBLIC_URL + '/iframe_window.html'}
+            frameborder="1"
+            title='game'
+            width={"640px"}
+            height={"480px"}
+            max-width={"100%"}> 
+        </iframe> 
+    </div>
+  );
+}
+
+export default App;
