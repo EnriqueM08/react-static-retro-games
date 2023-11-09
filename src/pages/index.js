371e2387b1694c16e220c5907ecf1c651d0b4843
Enrique Mata <eric.mataiii@gmail.com>
2023-11-09 17:07:39 -0600

    Trying to create login

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,24 +1,53 @@
-import React, { useEffect, /*useState*/ } from 'react';
+import React, { /*useEffect,*/ useState } from 'react';
 import axios from 'axios';
 
-const Home = () => {
-    //const [posts, setPosts] = useState([]);
+var data;
 
-    useEffect(() => {
-        console.log("BEFORE")
-        axios.get('https://kind-sand-0ef3bd710.4.azurestaticapps.net/api/server-Connect')
-            .then(
-            response => 
-            {
-                //setPosts(response.data);
-                console.log(response.data.data);
-            }
-            )
-            .catch(error => {
-                console.error(error);
-            });
-        console.log("RAN");
-    }, []);
+async function loginUser(user, pass) {
+    axios.get('https://kind-sand-0ef3bd710.4.azurestaticapps.net/api/login', {
+        params: {
+            username: user,
+            password: pass
+        }
+    })
+    .then(
+    response => 
+    {
+        data = (response.data.data);
+    }
+    )
+    .catch(error => {
+        console.error(error);
+    });
+    return data; 
+}
+
+export default function Home(){
+
+    const [username, setUserName] = useState();
+    const [password, setPassword] = useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser(
+          username,
+          password
+        );
+        console.log(token);
+    }
+
+    // useEffect(() => {
+    //     axios.get('https://kind-sand-0ef3bd710.4.azurestaticapps.net/api/server-Connect')
+    //         .then(
+    //         response => 
+    //         {
+    //             data = (response.data.data);
+    //         }
+    //         )
+    //         .catch(error => {
+    //             console.error(error);
+    //         });
+    // }, []);
 
     return (
         <div className="mainContainer">
@@ -28,16 +57,22 @@ const Home = () => {
             <div>
                 Please login to Continue!
             </div>
-            {/* <ul>
-                {posts.map((post) => (
-                    <>
-                        <li key={post.id}>{post.title}</li>
-                        <p>{post.body}</p>
-                    </>
-                ))}
-            </ul> */}
+            <form onSubmit={handleSubmit}>
+                <div class="container">
+                    <div class="testing">
+                        <label for="uname"><b>Username: </b></label>
+                        <input type="text" placeholder="Enter Username" name="uname" required onChange={e => setUserName(e.target.value)}/>
+                    </div>
+                    <div class = "testing">
+                        <label for="psw"><b>Password  : </b></label>
+                        <input type="password" placeholder="Enter Password" name="psw" required onChange={e => setPassword(e.target.value)}/>
+                    </div>
+                    <div class = "testing">
+                        <button type="submit">Login</button>
+                    </div>
+                </div>
+
+            </form>
         </div>
     );
 };
- 
-export default Home;
