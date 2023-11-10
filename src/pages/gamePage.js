ecf78703709d348258ed2695f45d8b017e37099d
Enrique Mata <eric.mataiii@gmail.com>
2023-11-10 11:10:36 -0600

    Update gamePage.js

--- src/pages/gamePage.js
+++ src/pages/gamePage.js
@@ -19,9 +19,10 @@ const GamePage = () => {
 
     const rows = [];
     for (let i = 0; i < games.data.length; i++) {
+        console.log(games.data);
         // note: we are adding a key prop here to allow react to uniquely identify each
         // element in this array. see: https://reactjs.org/docs/lists-and-keys.html
-        rows.push(<div key={i}>${games.data[i]}</div>);
+        rows.push(<div key={i}>${games.data.gameName[i]}</div>);
     }
 
     return (
