1d2c2c457af0a77a0e0dfb590d1a28461451fa04
Enrique Mata <eric.mataiii@gmail.com>
2023-11-09 19:07:21 -0600

    Update useToken.js

--- src/components/useToken.js
+++ src/components/useToken.js
@@ -3,16 +3,19 @@ import { useState } from 'react';
 export default function useToken() {
     const getToken = () => {
         const tokenString = sessionStorage.getItem('token');
-        return tokenString?.token
+        const userToken = JSON.parse(tokenString);
+        return userToken?.token
     };
-    const [token, setToken] = useState();
 
-    const saveToken = tokenString => {
-        sessionStorage.setItem('token', tokenString);
-        setToken(tokenString.token);
-    }
+    const [token, setToken] = useState(getToken());
+
+    const saveToken = userToken => {
+        sessionStorage.setItem('token', JSON.stringify(userToken));
+        setToken(userToken.token);
+    };
 
     console.log(getToken);
+    
     return {
         setToken: saveToken,
         token
