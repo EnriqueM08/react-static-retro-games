f5572e7820285bd2df7b8db4e392796f50ef201e
Enrique Mata <eric.mataiii@gmail.com>
2023-11-10 12:28:14 -0600

    Adding images

--- src/pages/gamePage.js
+++ src/pages/gamePage.js
@@ -28,7 +28,7 @@ const GamePage = () => {
         toPush = 
         <div class = "column">
             <div class="card">
-                <img src={require('../images/pkmRed.jpg')} alt="Pokemon" style={{width:"100%"}}/>
+                <img src={curRow.pictureLocation} alt="Pokemon" style={{width:"100%"}}/>
                 <div key={i}>{curRow.gameName}</div>
                 <p class="platform">{curRow.gameDevice}</p>
                 <p>{curRow.developer}</p>
