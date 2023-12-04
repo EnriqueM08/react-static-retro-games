9ce9448a38f1a6fdfc4ac15a9dc772989a247882
Enrique Mata <eric.mataiii@gmail.com>
2023-12-04 15:21:20 -0600

    Update signup.js

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -57,7 +57,7 @@ export default function SignUp(props) {
 				let temp = response.data.message;
 				let id = temp[0];
 				let idNum = id.id;
-				console.log(idNum);
+				console.log(JSON.stringify(idNum));
                 sessionStorage.setItem('token', idNum);
                 props.handleToken(response.data.message);
             }
